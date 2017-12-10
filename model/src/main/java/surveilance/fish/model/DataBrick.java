2d878c008d4dfc11e54d184d58af5ef258fdbc5d
No Name <spamthis333@yahoo.com>
2017-12-10 20:06:48 +0200

    Minor refactoring

--- model/src/main/java/surveilance/fish/model/DataBrick.java
+++ model/src/main/java/surveilance/fish/model/DataBrick.java
@@ -2,7 +2,7 @@ package surveilance.fish.model;
 
 public class DataBrick {
 
-    /** AES key used to encrypt the payload. The key is encrypted with a private RSA key and Base64 encoded. */
+    /** AES key used to encrypt the payload. The key is encrypted with a RSA key and Base64 encoded. */
     private String aesKey;
     
     /** Encrypted with the AES key and Base64 encoded. */
